2be8871f4f31871f2cdda349183723528312599b
Aleksei <Aleksei@ya.ru>
2021-09-08 22:32:58 +0300

    HW2

---
+++ src/main/java/HW2/ВecommissionedBus.java
@@ -0,0 +1,39 @@
+package HW2;
+
+public class MiniBus extends BusPark {
+
+    String compactbus;
+
+    public MiniBus() {
+    }
+
+    public MiniBus(String compactbus) {
+        this.compactbus = compactbus;
+    }
+
+    public MiniBus(String tsMark, String tsType, Integer tsNumber, Integer tsFuel, Integer tsCost,
+                   String compactbus) {
+        super(tsMark, tsType, tsNumber, tsFuel, tsCost);
+        this.compactbus = compactbus;
+    }
+
+    public String getCompactbus() {
+        return compactbus;
+    }
+
+    public void setCompactbus(String compactbus) {
+        this.compactbus = compactbus;
+    }
+
+    @Override
+    public String toString() {
+        return "Микро автобус"
+            + " Марка: " + tsMark
+            + ", Тип: " + tsType
+            + ", Количество: " + tsNumber
+            + " Расход: " + tsFuel
+            + ", Стоимость за 1 ед.: " + tsCost
+            + ", Компактный автобус: " + compactbus
+            ;
+    }
+}
